44e65de1286d124a1ad1fdf25d967cb062a2b6d2
suhalip <99282369+suhalip@users.noreply.github.com>
2023-02-14 14:58:25 -0500

    send button is working but the bubbles are on the wrong side.

--- app/src/main/java/com/example/snhuchat/MainActivity.java
+++ app/src/main/java/com/example/snhuchat/MainActivity.java
@@ -1,14 +1,111 @@
 package com.example.snhuchat;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+//import com.android.volley.Request;
+//import com.android.volley.RequestQueue;
+//import com.android.volley.Response;
+//import com.android.volley.VolleyError;
+//import com.android.volley.toolbox.JsonObjectRequest;
+//import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userMsgEdt;
+    private final String BOT_KEY = "bot";
+
+    // creating a variable for array list and adapter class.
+    private ArrayList<MessageModal> messageModalArrayList;
+    private MessageRVAdapter messageRVAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // on below line we are initializing all our views.
+        // creating variables for our
+        // widgets in xml file.
+        RecyclerView chatsRV = findViewById(R.id.idRVChats);
+        ImageButton sendMsgIB = findViewById(R.id.idIBSend);
+        userMsgEdt = findViewById(R.id.idEdtMessage);
+
+        // below line is to initialize our request queue.
+        // creating a variable for
+        // our volley request queue.
+        //RequestQueue mRequestQueue = Volley.newRequestQueue(MainActivity.this);
+        //mRequestQueue.getCache().clear();
+
+        // creating a new array list
+        messageModalArrayList = new ArrayList<>();
+
+        // adding on click listener for send message button.
+        sendMsgIB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // checking if the message entered
+                // by user is empty or not.
+                if (userMsgEdt.getText().toString().isEmpty()) {
+                    // if the edit text is empty display a toast message.
+                    Toast.makeText(MainActivity.this, "Please enter your message..", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // calling a method to send message
+                // to our bot to get response.
+                sendMessage(userMsgEdt.getText().toString());
+
+                // below line we are setting text in our edit text as empty
+                userMsgEdt.setText("");
+            }
+        });
+
+        // on below line we are initializing our adapter class and passing our array list to it.
+        messageRVAdapter = new MessageRVAdapter(messageModalArrayList, this);
+
+        // below line we are creating a variable for our linear layout manager.
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this, RecyclerView.VERTICAL, false);
+
+        // below line is to set layout
+        // manager to our recycler view.
+        chatsRV.setLayoutManager(linearLayoutManager);
+
+        // below line we are setting
+        // adapter to our recycler view.
+        chatsRV.setAdapter(messageRVAdapter);
+    }
+
+    private void sendMessage(String userMsg) {
+        // below line is to pass message to our
+        // array list which is entered by the user.
+        String USER_KEY = "user";
+        messageModalArrayList.add(new MessageModal(userMsg, USER_KEY));
+        messageRVAdapter.notifyDataSetChanged();
+
+        // url for our brain
+        // make sure to add mshape for uid.
+        // make sure to add your url.
+        String url = "Enter you API URL here" + userMsg;
+
+        // creating a variable for our request queue.
+        //RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+        // on below line we are making a json object request for a get request and passing our url .
+
+        // at last adding json object
+        // request to our queue.
     }
 }
