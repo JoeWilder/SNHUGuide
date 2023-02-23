d20a7ad392daa0f3287b0f6c5bccd41d60b0c829
Suhali <suhali.patel@snhu.edu>
2023-02-23 13:42:49 -0500

    Color changes

--- app/src/main/java/com/example/snhuchat/MainActivity.java
+++ app/src/main/java/com/example/snhuchat/MainActivity.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private EditText userMsgEdt;
-    private final String BOT_KEY = "bot";
 
     // creating a variable for array list and adapter class.
     private ArrayList<MessageModal> messageModalArrayList;
@@ -49,42 +48,37 @@ public class MainActivity extends AppCompatActivity {
         //RequestQueue mRequestQueue = Volley.newRequestQueue(MainActivity.this);
         //mRequestQueue.getCache().clear();
 
-        // creating a new array list
         messageModalArrayList = new ArrayList<>();
 
         // adding on click listener for send message button.
         sendMsgIB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // checking if the message entered
-                // by user is empty or not.
+                // checking if the message entered by user is empty or not.
                 if (userMsgEdt.getText().toString().isEmpty()) {
                     // if the edit text is empty display a toast message.
                     Toast.makeText(MainActivity.this, "Please enter your message..", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                // calling a method to send message
-                // to our bot to get response.
+                // calling a method to send message to our bot to get response.
                 sendMessage(userMsgEdt.getText().toString());
 
-                // below line we are setting text in our edit text as empty
+                //setting text in our edit text as empty
                 userMsgEdt.setText("");
             }
         });
 
-        // on below line we are initializing our adapter class and passing our array list to it.
+        //initializing our adapter class and passing our array list to it.
         messageRVAdapter = new MessageRVAdapter(messageModalArrayList, this);
 
-        // below line we are creating a variable for our linear layout manager.
+        //creating a variable for our linear layout manager.
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this, RecyclerView.VERTICAL, false);
 
-        // below line is to set layout
-        // manager to our recycler view.
+        // layout manager to our recycler view.
         chatsRV.setLayoutManager(linearLayoutManager);
 
-        // below line we are setting
-        // adapter to our recycler view.
+        // setting adapter to our recycler view.
         chatsRV.setAdapter(messageRVAdapter);
     }
 
@@ -98,8 +92,11 @@ public class MainActivity extends AppCompatActivity {
         // url for our brain
         // make sure to add mshape for uid.
         // make sure to add your url.
-        String url = "Enter you API URL here" + userMsg;
+
+
+        //String url = "Enter you API URL here" + userMsg;
         String response = "Good Morning! How are you";
+        String BOT_KEY = "bot";
         if(userMsgEdt.getText().toString().contains("Hi ")){
             messageModalArrayList.add(new MessageModal("Hello", BOT_KEY));
         }
