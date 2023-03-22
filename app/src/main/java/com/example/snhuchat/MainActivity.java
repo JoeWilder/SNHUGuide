c6126c45493500394e0165495755b4ec63302cb6
Suhali <suhali.patel@snhu.edu>
2023-03-22 14:45:49 -0400

    Title and images added

--- app/src/main/java/com/example/snhuchat/MainActivity.java
+++ app/src/main/java/com/example/snhuchat/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.snhuchat;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -8,7 +7,9 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -36,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         bot = new DialogflowBot(this);
 
         map = new CampusMap(getApplicationContext());
